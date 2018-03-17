2398c5085a5eb2d9f85646473a3a5796b37d6050
mojojojo <costin.genescu@gmail.com>
2018-03-17 21:27:07 +0200

    playground

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PiPython',
+    version='',
+    packages=['modules', 'modules.pplayer', 'modules.playground'],
+    url='',
+    license='',
+    author='mojojojo',
+    author_email='',
+    description=''
+)
